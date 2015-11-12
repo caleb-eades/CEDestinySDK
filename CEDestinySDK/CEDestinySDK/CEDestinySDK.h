a51c8dcb9589530cdd3c4891445c2ecd0bcfca19
Caleb Eades <caleb.eades@mindtree.com>
2015-11-12 14:33:14 -0500

    Initial Commit

---
+++ CEDestinySDK/CEDestinySDK/CEDestinySDK.h
@@ -0,0 +1,19 @@
+//
+//  CEDestinySDK.h
+//  CEDestinySDK
+//
+//  Created by Caleb Eades on 11/12/15.
+//  Copyright (c) 2015 Caleb Eades. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CEDestinySDK.
+FOUNDATION_EXPORT double CEDestinySDKVersionNumber;
+
+//! Project version string for CEDestinySDK.
+FOUNDATION_EXPORT const unsigned char CEDestinySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CEDestinySDK/PublicHeader.h>
+
+
